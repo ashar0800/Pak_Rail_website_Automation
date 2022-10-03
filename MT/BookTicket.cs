0becb426ad357a85185f83e7b50b21757b6fdf48
Ashar Nasir <asharnasir0800@gmail.com>
2022-10-03 11:00:28 +0500

    Update BookTicket.cs

--- MT/BookTicket.cs
+++ MT/BookTicket.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,51 +8,76 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 
 namespace MT
 {
-    public class BookTicket
+    public class BookTicket:BaseClass
     {
+
+        #region
+        By selectjourney = By.XPath("//*[@id='LeftPanel1_LeftPanelInternal1_ddlTripType']");
+
+        By fromstation = By.XPath("//*[@id='LeftPanel1_LeftPanelInternal1_ddFromStation']");
+        
+        By tostation = By.XPath("//*[@id='LeftPanel1_LeftPanelInternal1_ddToStation']");
+
+        By submitButton = By.XPath("//*[@id='LeftPanel1_LeftPanelInternal1_btnPlanJourney']");
+        
+        By checkSeats = By.XPath("//*[@id='rptTrains_btnCheckSeats_0']");
+        By departureDate = By.XPath("//*[@id='txtDepartureDate']");
+        By selectDate = By.XPath("/html/body/div/div[1]/table/tbody/tr[6]/td[6]");
+
+        By searchTrains = By.XPath("//*[@id='btnSearch']");
+        By searchSeats = By.XPath("//*[@id='ACL49_seats']/span");
+
+        #endregion
         public void bookTicketViaLogin()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://pakrail.gov.pk/";
-            
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            implicitWait(10);
             Thread.Sleep(3000);
-            driver.FindElement(By.XPath("//*[@id='TopControl1_ancSignIn']")).Click();
-            driver.FindElement(By.CssSelector("#txtMobile")).SendKeys("923405619771");
-            driver.FindElement(By.CssSelector("#txtPassword")).SendKeys("asharnasir0800");
+            
+            
+            Login log=new Login();
+            
+            log.loginTest();
+            implicitWait(10);
 
-            Thread.Sleep(10000);
-            driver.FindElement(By.XPath("//*[@id='btn_Submit']")).Click();
-            Thread.Sleep(1000);
-            driver.FindElement(By.XPath("//*[@id='eTicketWarning']/div/div/div[1]/button")).Click();
-
-            driver.FindElement(By.CssSelector("#planYourJourney > div.col-md-12.col-lg-12 > div:nth-child(3) > div > button > span.filter-option.pull-left")).Click();
-            driver.FindElement(By.XPath("//*[@id='planYourJourney']/div[1]/div[1]/div/div/ul/li[2]/a")).Click();
-
-            Thread.Sleep(1000);
-            driver.FindElement(By.XPath("//*[@id='planYourJourney']/div[1]/div[2]/div/button/span[1]")).Click();
-            driver.FindElement(By.XPath("//*[@id='planYourJourney']/div[1]/div[2]/div/div/ul/li[19]/a/span[1]")).Click();
+            new SelectElement(findElement(selectjourney)).SelectByIndex(1);
+            implicitWait(10);
             Thread.Sleep(1000);
 
-            driver.FindElement(By.XPath("//*[@id='planYourJourney']/div[1]/div[3]/div/button/span[1]")).Click();
-            driver.FindElement(By.XPath("//*[@id='planYourJourney']/div[1]/div[3]/div/div/ul/li[138]/a/span[1]")).Click();
+            new SelectElement(findElement(fromstation)).SelectByIndex(137);
+            implicitWait(10);
             Thread.Sleep(1000);
 
-            driver.FindElement(By.XPath("//*[@id='LeftPanel1_LeftPanelInternal1_btnPlanJourney']")).Click();
-            Thread.Sleep(1000);
-            driver.FindElement(By.XPath("//*[@id='rptTrains_btnCheckSeats_0']")).Click();
+            new SelectElement(findElement(tostation)).SelectByIndex(18);
+            implicitWait(10);
             Thread.Sleep(1000);
 
-            driver.FindElement(By.XPath("//*[@id='txtDepartureDate']")).Click();
+            myClick(submitButton);
+            implicitWait(10);
             Thread.Sleep(1000);
-            driver.FindElement(By.XPath("/html/body/div/div[1]/table/tbody/tr[5]/td[6]")).Click();
+
+            myClick(checkSeats);
+            implicitWait(10);
             Thread.Sleep(1000);
-            driver.FindElement(By.XPath("//*[@id='btnSearch']")).Click();
+
+            myClick(departureDate);
+            implicitWait(10);
             Thread.Sleep(1000);
-            driver.FindElement(By.XPath("//*[@id='ACL48_seats']/span")).Click();
+
+            myClick(selectDate);
+            implicitWait(10);
+            Thread.Sleep(1000);
+            
+            myClick(searchTrains);
+            implicitWait(10);
+            Thread.Sleep(1000);
+            
+            myClick(searchSeats);
+            implicitWait(10);
             Thread.Sleep(1000);
         }
     }
