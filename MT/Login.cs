791166010160781ceecee05c3d30416065df0e70
Ashar Nasir <asharnasir0800@gmail.com>
2022-10-03 10:57:18 +0500

    Update Login.cs

--- MT/Login.cs
+++ MT/Login.cs
@@ -1,11 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
@@ -21,21 +20,22 @@ namespace MT
         By submitButton = By.XPath("//*[@id='btn_Submit']");
         By removeWarning = By.XPath("//*[@id='eTicketWarning']/div/div/div[1]/button");
         #endregion
-
-        public void logintest(string mobile, string password)
-        {
-            IWebDriver driver=new ChromeDriver(); 
-            URL("https://pakrail.gov.pk/");
+        
+        public void loginTest()
+        { 
             maximizeWindow();
             implicitWait(10);
             Thread.Sleep(3000);
 
             myClick(loginButton);    
-            myText(Phone, mobile);
-            myText(Password, password);
+            
+            myText(Phone, userMobile);
+            myText(Password, userPassword);
             Thread.Sleep(10000);
+
             myClick(submitButton);
             Thread.Sleep(1000);
+            
             myClick(removeWarning);
         }
 
