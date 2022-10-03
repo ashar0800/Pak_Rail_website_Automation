5fc9051d4085b6613b9b3cc349f093f97bf87095
Ashar Nasir <asharnasir0800@gmail.com>
2022-10-03 10:43:20 +0500

    Update CheckFare.cs

--- MT/CheckFare.cs
+++ MT/CheckFare.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,40 +16,50 @@ namespace MT
     public class CheckFare:BaseClass
     {
         #region
-        By showdropdown1 = By.CssSelector("#planYourJourney > div.col-md-12.col-lg-12 > div:nth-child(3) > div > button > span.filter-option.pull-left");
-        By clickdropdown1 = By.XPath("//*[@id='planYourJourney']/div[1]/div[1]/div/div/ul/li[2]/a");
+        By selectjourney = By.XPath("//*[@id='LeftPanel1_LeftPanelInternal1_ddlTripType']");
 
-        By showdropdown2 = By.XPath("//*[@id='planYourJourney']/div[1]/div[2]/div/button/span[1]");
-        By clickdropdown2 = By.XPath("//*[@id='planYourJourney']/div[1]/div[2]/div/div/ul/li[19]/a/span[1]");
+        By fromstation = By.XPath("//*[@id='LeftPanel1_LeftPanelInternal1_ddFromStation']");
 
-        By showdropdown3 = By.XPath("//*[@id='planYourJourney']/div[1]/div[3]/div/button/span[1]");
-        By clickdropdown3 = By.XPath("//*[@id='planYourJourney']/div[1]/div[3]/div/div/ul/li[138]/a/span[1]");
+        By tostation = By.XPath("//*[@id='LeftPanel1_LeftPanelInternal1_ddToStation']");
+
+
+        By selecttrain = By.XPath("//*[@id='LeftPanel1_LeftPanelInternal1_ddFareTrains']");
+
+        By fromstationdropdown = By.XPath("//*[@id='LeftPanel1_LeftPanelInternal1_ddFareFromStation']");
+
+        By tostationdropdown = By.XPath("//*[@id='LeftPanel1_LeftPanelInternal1_ddFareToStation']");
 
         By submitButton = By.XPath("//*[@id='LeftPanel1_LeftPanelInternal1_btnPlanJourney']");
         By checkFareButton = By.XPath("//*[@id='rptTrains_btnCheckFare_0']");
 
         By checkFareValidity = By.XPath("//*[@id='form1']/div[3]/div[5]/div/div[2]/div[3]/div/table/tbody/tr[2]/td[2]");
+
+
+
+
+       
+
+        By getfareButton = By.XPath("//*[@id='LeftPanel1_LeftPanelInternal1_btnGetFare']");
+        By checkSeatFareValidity = By.XPath("//*[@id='form1']/div[3]/div[5]/div/div[2]/div[3]/div/table/tbody/tr[2]/td[3]");
+
         #endregion
-        public void checkFare(int fare)
+        public void checkFareViaRoute()
         {
             
             maximizeWindow();
             implicitWait(10);
 
-
-            myClick(showdropdown1);
-            myClick(clickdropdown1);
+            new SelectElement(findElement(selectjourney)).SelectByIndex(1);
 
             Thread.Sleep(1000);
 
-            myClick(showdropdown2);
-            myClick(clickdropdown2);
-            
+            new SelectElement(findElement(fromstation)).SelectByIndex(137);
+
             Thread.Sleep(1000);
 
-            myClick(showdropdown3);
-            myClick(clickdropdown3);
-                Thread.Sleep(1000);
+            new SelectElement(findElement(tostation)).SelectByIndex(18);
+
+            Thread.Sleep(1000);
 
             myClick(submitButton);
             Thread.Sleep(1000);
@@ -58,7 +68,30 @@ namespace MT
             Thread.Sleep(1000);
 
             
-            Assert.AreEqual(findElement(checkFareValidity).Text, fare.ToString());
+            //Assert.AreEqual(findElement(checkFareValidity).Text, fare.ToString());
+        }
+
+        public void checkFareViaTrain()
+        {
+
+            maximizeWindow();
+            implicitWait(10);
+
+
+            new SelectElement(findElement(selecttrain)).SelectByIndex(1);
+
+            Thread.Sleep(1000);
+
+            new SelectElement(findElement(fromstationdropdown)).SelectByIndex(1);
+
+            Thread.Sleep(1000);
+
+            new SelectElement(findElement(tostationdropdown)).SelectByIndex(1);
+
+            Thread.Sleep(1000);
+
+            //int fare=1950;
+            //Assert.AreEqual(findElement(checkSeatFareValidity).Text, fare.ToString());
         }
     }
 }
